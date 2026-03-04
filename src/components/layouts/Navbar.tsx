4cbd74a0c852f0c708fa0062388d9b90a8639e4a
Jahangir-alam25 <jahangiralam10022003@gmail.com>
2026-03-04 13:58:20 +0600

    added authentication page structure

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -66,7 +66,7 @@ export default function Navbar() {
         </div>
 
         <button className="bg-yellow-400 px-6 py-2 rounded font-semibold">
-          Login
+<a href="/auth">Login</a>
         </button>
       </div>
 
